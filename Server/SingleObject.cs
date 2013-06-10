ea9c0eb197ec916f3cba1d5cf405b9904c4af4c4
KaMyLuS <kamil.wajda1@gmail.com>
2013-06-10 10:45:03 +0200

    Dodano puste klasy pod niektore czesci aplikacji

---
+++ Server/SingleObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Server
+{
+    class SingleObject
+    {
+        // pozycja srodka obiektu wg wspolrzednych Kinecta
+        private Point centroidPosition;
+
+        private string objectName;
+        private string objectType;
+
+        // pozycja srodka obiektu wg wspolrzednych ekranu docelowego
+        Point ScreenCentroidPosition
+        {
+            get
+            {
+                // TO DO !!
+                return new Point(0, 0);
+            }
+        }
+
+
+        SingleObject(string type, string name, double x = 0, double y = 0)
+        {
+            this.centroidPosition = new Point(x, y);
+            this.objectType = type;
+            this.objectName = name;
+        }
+
+        SingleObject(string type, string name, Point pos)
+        {
+            this.centroidPosition = pos;
+            this.objectType = type;
+            this.objectName = name;
+        }
+
+        public void MoveTo(double x, double y)
+        {
+            this.centroidPosition.X = x;
+            this.centroidPosition.Y = y;
+        }
+
+        public void MoveTo(Point newPos)
+        {
+            this.centroidPosition = newPos;
+        }
+
+        public Point GetCentroidPosition()
+        {
+            return centroidPosition;
+        }
+    }
+}
